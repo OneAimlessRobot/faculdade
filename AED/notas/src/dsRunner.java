a4b287e452a461de2c696899b834126b5f797b72
OneAimlessRobot <adri.campos.valente@gmail.com>
2023-10-03 20:05:40 +0100

    sajadsdasjdajajsddajajdjdadja

--- AED/notas/src/dsRunner.java
+++ AED/notas/src/dsRunner.java
@@ -1,15 +1,18 @@
 import notas.ds.exceptions.*;
 import notas.ds.implem.*;
 import notas.ds.interfaces.*;
+import notas.ds.interfaces.Queue;
+import dataStructures.*;
+import dataStructures.EmptyQueueException;
 
 import java.io.IOException;
 
 import notas.auxiliar.*;
 public class dsRunner {
 
-	public static void main(String[] args) throws IOException, EmptyQueueException {
+	public static void main(String[] args) throws IOException, EmptyQueueException, notas.ds.exceptions.EmptyQueueException {
 
-		QueueInFixedArray<Integer> intQueue= new QueueInFixedArray<>(5);
+		Queue<Integer> intQueue= new QueueInFixedArray<>(5);
 		Integer[] nums= CollectionAlgorithms.randIntArrAux(0, 100, 3);
 		
 		for(int i=0;i<nums.length;i++) {
