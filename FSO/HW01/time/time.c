2295f71d9357b9134c33505b62663b26bbfb0b1c
OneAimlessRobot <adri.campos.valente@gmail.com>
2023-10-06 00:15:59 +0100

    21222

--- FSO/HW01/time/time.c
+++ FSO/HW01/time/time.c
@@ -12,30 +12,67 @@
 
 #define BILLION  1000000000.0
 
+void trim (char *dest, char *src)
+{
+    if (!src || !dest)
+       return;
+
+    int len = strlen (src);
+
+    if (!len) {
+        *dest = '\0';
+        return;
+    }
+    char *ptr = src + len - 1;
+
+    // remove trailing whitespace
+    while (ptr > src) {
+        if (!isspace (*ptr))
+            break;
+        ptr--;
+    }
+
+    ptr++;
+
+    char *q;
+    // remove leading whitespace
+    for (q = src; (q < ptr && isspace (*q)); q++)
+        ;
+
+    while (q < ptr)
+        *dest++ = *q++;
+
+    *dest = '\0';
+}
 
 int main(int argc, char**argv) {
 
 	if(argc<2){
 
-		perror("Precisas de um comando!!!\n");
+		perror("argv[1]");
 		exit(-1);
 
 	}
 	int fd,redirect=0;
-	char* filePath,*filename,buff[1024];
+	char* filePath,buff[1024],buff2[1024];
 
 	for(int i=0;i<argc;i++){
 
 		if(argv[i][0]=='='){
 			redirect=1;
-			filename= &argv[i][1];
-			filePath=strcat(strcat(getcwd(buff,1024),"/"),filename);
-			int j=i;
-			for(;j<argc-1;j++){
+			if(strlen(argv[i])>1){
 
-				argv[j]=argv[j+1];
+			strcpy(buff2,&argv[i][1]);
 			}
-			argv[j]=0;
+			else{
+			trim(buff2,&argv[i+1][0]);
+			
+			argv[i+1]=0;
+			
+			}
+			argv[i]=0;
+			filePath=strcat(strcat(getcwd(buff,1024),"/"),buff2);
+			
 			break;
 
 		}
@@ -47,21 +84,24 @@ int main(int argc, char**argv) {
 
 	int pid=fork();
 	switch(pid){
+		case -1:
+			perror("fork");
+			exit(-1);
 
 		case 0:
 			if(redirect){
 			fd=creat(filePath,0666);
 			if(fd<0){
 
-				perror("erro ao abrir ficheiro\n");
+				perror("creat");
 				exit(-1);
 			}
 			dup2(fd,1);
 			
 			}
 			execvp(argv[1],argv+1);
-			perror("erro no exec. Processo nao mudou.");
-			exit(1);
+			perror("exec");
+			exit(-1);
 
 			
 		default:{
@@ -78,7 +118,7 @@ int main(int argc, char**argv) {
     double time_spent = (end.tv_sec - start.tv_sec) +
                         (end.tv_nsec - start.tv_nsec) / BILLION;
  
-    printf("\nElapsed time: %6f seconds\n", (float)time_spent);
+    printf("\nThe elapsed time is %lf seconds\n", time_spent);
 
 		
 
